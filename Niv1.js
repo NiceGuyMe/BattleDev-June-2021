8a4d6375103f8a4ba3148bc91da9f0e4b4234a11
Loïca <95331041+NiceGuyMe@users.noreply.github.com>
2022-03-02 08:20:38 +0300

    Update Niv1.js

--- Niv1.js
+++ Niv1.js
@@ -1,9 +1,9 @@
 const prompt = require('prompt-sync')();
 // input
-var D = +prompt(`saisir la masse d’ergol nécessaire au décollage en kg.`)
-var L = +prompt('saisir la distance total du voyage en UA')
+var D = +prompt(`enter the propellant weight required for take-off in kg.`)
+var L = +prompt('enter the total distance of the trip in AU')
 //output
 const Calcul = () => (D + L * 5);
 console.log("--------------------------------------------");
-console.log(`la masse total pour le voyage est ${Calcul()} kg`);
+console.log(`the total mass for the journey is${Calcul()} kg`);
 console.log("--------------------------------------------");
